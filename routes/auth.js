23b621daae4886d890cdfb33352c9c271f9e5fe5
Elena Yadryshnikova <elenaya014@gmail.com>
2023-07-20 01:29:14 +0700

    change logout to post

--- routes/auth.js
+++ routes/auth.js
@@ -23,6 +23,6 @@ router.post('/signin', celebrate({
   }),
 }), controller.login);
 
-router.get('/signout', controller.logout);
+router.post('/signout', controller.logout);
 
 module.exports = router;
